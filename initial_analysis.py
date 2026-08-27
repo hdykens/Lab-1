c894fa52072f233f0839b021e421fd9e08508bca
HSweazey <hrsweazey@gmail.com>
2026-08-27 12:16:10 -0400

    Moved files and intial analysis

---
+++ initial_analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('ForeignGifts_edu.csv')
+print(df.head())
+
+# 1. Load the Foreign Gifts data.
+#      2. Describe the differences between different classes of gift types.
+#      3. Answer the following questions:
+#          1. Which country gives the most money in total?
+#          2. Which country initiates the most gifts by count?
+#          3. Which country gives the largest gifts on average?
+#          4. Which institution receive the most money in total?
+#          5. Which institution receive the greatest number of gifts by count?
+#          6. What is the average amount each university receives? Median amount? If they are different why?
+#          7. What is the largest flow of a single country to a single institution?
+#          8. Which are the top giftors to US academic institutions?
